6b3c3b7b2390e4f6682f5de49462e8292c74bcb6
Anna Shipunova <tetya-loshad@yandex.ru>
2010-06-09 11:20:42 -0400

    py server - no assync

--- py_server/neti_s_server.py
+++ py_server/neti_s_server.py
@@ -24,31 +24,52 @@ class MyTCPHandler(SocketServer.StreamRequestHandler):
 	#     client.
 	#     """
 	
+	# global total_data
+	# data = conn.recv(1024)
+	# if len(data) < 1024:
+	# 	total_data = total_data + data
+	# 	conn_name = conn.getpeername()
+	# 	print "NetiNeti: Connection %s closed." % conn_name[1]
+	# 	t2 = time.clock()
+	# 	t = t2 - t_connected
+	# 	print t
+	# 	conn.send(nf.find_names(total_data))
+	# 	# print total_data
+	# 	total_data = ""
+	# 	return False
+	# else:
+	# 	total_data = total_data + data
+	# 	return True
+	
+	
     def handle(self):
         # self.request is the TCP socket connected to the client
-        total_data = []
+        global total_data
         conn = self.request
         conn_name = conn.getpeername()
-
-        print "Connected %s" % conn_name[1]
+        print "NetiNeti: Connected %s" % conn_name[1]
         while 1:
 	            data = self.request.recv(1024)
+	            print "NetiNeti: data =  %s" % data
 	            if len(data) < 1024:
-	                total_data.append(data)
+	                total_data = total_data + data
 	                break
-	            total_data.append(data)
-        t_data = ''.join(total_data)
-        time.sleep(2)
-        self.request.send(nf.find_names(t_data))
-        print "Connection %s closed." % conn_name[1]
+	            else:
+	                total_data = total_data + data
+
+        # time.sleep(2)
+        print "NetiNeti: total_data =  %s" % total_data
+        self.request.send(nf.find_names(total_data))
+        total_data = ""
+        print "NetiNeti: Connection %s closed." % conn_name[1]
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 1234
 
     # Create the server, binding to localhost on port 1234
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
-    print "Initialize server and start listening."
+    total_data = ""
+    print "NetiNeti: Initialize server and start listening."
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
--- sinatra/spec/sinatra_web_serv_spec.rb
+++ sinatra/spec/sinatra_web_serv_spec.rb
@@ -41977,6 +41977,7 @@ describe 'The Neti Neti ' do
     it "should show correct first page" do
       get "/tf_result"
       # last_response.body.should include("<tr><td>B. rufimanus</td><td>Bruchus rufimanus</td></tr>")    
+      last_response.should be_ok
       last_response.body.should include("<a href=\"http://example.org/tf_result?page=7\">7</a>")
       last_response.body.should include('<span class="previous_page disabled">')
       last_response.body.should include("href=\"http://example.org/tf_result?page=2\"")
@@ -41984,6 +41985,7 @@ describe 'The Neti Neti ' do
     end    
   
     it "should show correct last page" do
+      last_response.should be_ok
       get "tf_result?page=7"
       # last_response.body.should include('<tr><td>Xestohium riifovillo-</td><td>Xestohium riifovillo-</td></tr>')
       last_response.body.should include('<span class="next_page disabled">')
@@ -41994,6 +41996,7 @@ describe 'The Neti Neti ' do
     it "should show correct middle page" do
       get "tf_result?page=5"
       # last_response.body.should include('<tr><td>Osmylus chrysops</td><td>Osmylus chrysops</td></tr>')
+      last_response.should be_ok
       last_response.body.should include('<em>5</em>')
       last_response.body.should include("href=\"http://example.org/tf_result?page=4\"")
       last_response.body.should include("&#8592; Previous</a>")  
@@ -42003,18 +42006,21 @@ describe 'The Neti Neti ' do
 
     it "should show first page if page number is bad" do
       get "tf_result?page=-1"
+      last_response.should be_ok
       last_response.body.should include('<em>1</em>')
       last_response.body.should include("<a href=\"http://example.org/tf_result?page=-1?page=7\">7</a>")
     end
 
     it "should show first page if page number is not a number" do
       get "tf_result?page=foo"
+      last_response.should be_ok
       last_response.body.should include('<em>1</em>')
       last_response.body.should include("<a href=\"http://example.org/tf_result?page=foo?page=7\">7</a>")
     end
 
     it "should show first page if page number is bigger then total amount of pages" do
       get "tf_result?page=100"
+      last_response.should be_ok
       last_response.body.should include('<em>1</em>')
       last_response.body.should include("<a href=\"http://example.org/tf_result?page=7\">7</a>")
     end
@@ -42022,6 +42028,7 @@ describe 'The Neti Neti ' do
     it 'should show no pagination for small text' do
       post "/tf_result", params = {"url"=>@URL, "url_e"=>"none", "text"=>""}
       get "tf_result"
+      last_response.should be_ok
       last_response.body.should_not include("<a href=\"http://example.org/tf_result?page=")
       last_response.body.should_not include("&#8592; Previous</a>")  
       last_response.body.should_not include("Next &#8594;</a>")
@@ -42029,103 +42036,103 @@ describe 'The Neti Neti ' do
   end
 end
 
-describe 'The Reconcile App' do
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
-
-  before :all do
-    @bad_URL    = "http://localhost/text_bad.txt"
-    @good_URL   = "http://localhost/text_good.txt" 
-    @long_URL   = "http://localhost/pictorialgeo.txt"
-    @wrong_URL1 = "http://localhost/text_good.txt%20%20%20"
-    @wrong_URL2 = "http://localhost/text_good.txt   "
-    @not_url    = "a"
-    @text1 = URI.escape "Atys sajidersoni\nAhys sandersoni"
-    @text2 = URI.escape "Atys sandersoni"
-    @text3 = URI.escape "Atys sajidersoni\n\rAhys sandersoni"
-    @upload1 = Rack::Test::UploadedFile.new '/Library/Webserver/Documents/text_bad.txt'
-    @upload2 = Rack::Test::UploadedFile.new '/Library/Webserver/Documents/text_good.txt'
-  end
-
-  it "check index html" do
-    get '/'
-#    last_response.should be_ok
-    last_response.body.should include("Neti Neti Taxon Finder") 
-    last_response.body.should include("Scientific Names Reconciliation") 
-  end
-
-  it "check reconciliation form html" do
-    get '/recon'
-#    last_response.should be_ok
-    last_response.body.should include("Nlist2.txt")
-  end
-
-  it "should take url and return text" do
-    post "/submit", params = {"url1"=>@bad_URL, "url2"=>@good_URL, "url_e"=>"none", "freetext1"=>"", "freetext2"=>""}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-  
-  it "should take both texts and return text" do
-    post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"none", "freetext1"=>@text1, "freetext2"=>@text2}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-  
-  it "should upload 2 urls and return text" do
-    post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"", "freetext1"=>"", "freetext2"=>"", "upload1"=>@upload1, "upload2"=>@upload2}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-  
-  it "should take text and example url and return text" do
-    post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>@text1, "freetext2"=>""}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-
-  it "should take url and example url and return text" do
-    post "/submit", params = {"url1"=>@bad_URL, "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>""}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-
-  it "should upload file and example url and return text" do
-    post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>"", "upload1"=>@upload1}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-  
-  it "should take text with \r\n and return text" do
-    post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"none", "freetext1"=>@text3, "freetext2"=>@text1}
-#    last_response.should be_ok
-    last_response.body.should include("<td>Atys sajidersoni</td> <td>---></td> <td>Ahys sandersoni</td>")
-  end    
-
-  it "should take url wth spaces at the end and example url and return text" do
-    post "/submit", params = {"url1"=>@wrong_URL2, "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>""}
-    last_response.should be_ok
-    last_response.body.should include("<td>Abra aequalis</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-
-  it "should take url wth %20 at the end and example url and return text" do
-    post "/submit", params = {"url1"=>@wrong_URL1, "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>""}
-    last_response.should be_ok
-    last_response.body.should include("<td>Abra aequalis</td> <td>---></td> <td>Atys sandersoni</td>")
-  end    
-
-  it "should return err message if only one url was provided" do
-    post "/submit", params = {"url1"=>@bad_URL, "url2"=>"", "url_e"=>"", "freetext1"=>"", "freetext2"=>""}
-    last_response.should be_ok
-    last_response.body.should include("Sorry, something went wrong, please try again:")
-  end    
-
-  it "should return err message if URL isn't an URL" do
-    post "/submit", params = {"url1"=>@bad_URL, "url2"=>@not_URL, "url_e"=>"", "freetext1"=>"", "freetext2"=>""}
-    last_response.should be_ok
-    last_response.body.should include("Sorry, something went wrong, please try again:")
-  end    
-end
+# describe 'The Reconcile App' do
+#   include Rack::Test::Methods
+# 
+#   def app
+#     Sinatra::Application
+#   end
+# 
+#   before :all do
+#     @bad_URL    = "http://localhost/text_bad.txt"
+#     @good_URL   = "http://localhost/text_good.txt" 
+#     @long_URL   = "http://localhost/pictorialgeo.txt"
+#     @wrong_URL1 = "http://localhost/text_good.txt%20%20%20"
+#     @wrong_URL2 = "http://localhost/text_good.txt   "
+#     @not_url    = "a"
+#     @text1 = URI.escape "Atys sajidersoni\nAhys sandersoni"
+#     @text2 = URI.escape "Atys sandersoni"
+#     @text3 = URI.escape "Atys sajidersoni\n\rAhys sandersoni"
+#     @upload1 = Rack::Test::UploadedFile.new '/Library/Webserver/Documents/text_bad.txt'
+#     @upload2 = Rack::Test::UploadedFile.new '/Library/Webserver/Documents/text_good.txt'
+#   end
+# 
+#   it "check index html" do
+#     get '/'
+# #    last_response.should be_ok
+#     last_response.body.should include("Neti Neti Taxon Finder") 
+#     last_response.body.should include("Scientific Names Reconciliation") 
+#   end
+# 
+#   it "check reconciliation form html" do
+#     get '/recon'
+# #    last_response.should be_ok
+#     last_response.body.should include("Nlist2.txt")
+#   end
+# 
+#   it "should take url and return text" do
+#     post "/submit", params = {"url1"=>@bad_URL, "url2"=>@good_URL, "url_e"=>"none", "freetext1"=>"", "freetext2"=>""}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+#   
+#   it "should take both texts and return text" do
+#     post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"none", "freetext1"=>@text1, "freetext2"=>@text2}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+#   
+#   it "should upload 2 urls and return text" do
+#     post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"", "freetext1"=>"", "freetext2"=>"", "upload1"=>@upload1, "upload2"=>@upload2}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+#   
+#   it "should take text and example url and return text" do
+#     post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>@text1, "freetext2"=>""}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+# 
+#   it "should take url and example url and return text" do
+#     post "/submit", params = {"url1"=>@bad_URL, "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>""}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+# 
+#   it "should upload file and example url and return text" do
+#     post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>"", "upload1"=>@upload1}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Ahys sandersoni</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+#   
+#   it "should take text with \r\n and return text" do
+#     post "/submit", params = {"url1"=>"", "url2"=>"", "url_e"=>"none", "freetext1"=>@text3, "freetext2"=>@text1}
+# #    last_response.should be_ok
+#     last_response.body.should include("<td>Atys sajidersoni</td> <td>---></td> <td>Ahys sandersoni</td>")
+#   end    
+# 
+#   it "should take url wth spaces at the end and example url and return text" do
+#     post "/submit", params = {"url1"=>@wrong_URL2, "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>""}
+#     last_response.should be_ok
+#     last_response.body.should include("<td>Abra aequalis</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+# 
+#   it "should take url wth %20 at the end and example url and return text" do
+#     post "/submit", params = {"url1"=>@wrong_URL1, "url2"=>"", "url_e"=>"text_good.txt", "freetext1"=>"", "freetext2"=>""}
+#     last_response.should be_ok
+#     last_response.body.should include("<td>Abra aequalis</td> <td>---></td> <td>Atys sandersoni</td>")
+#   end    
+# 
+#   it "should return err message if only one url was provided" do
+#     post "/submit", params = {"url1"=>@bad_URL, "url2"=>"", "url_e"=>"", "freetext1"=>"", "freetext2"=>""}
+#     last_response.should be_ok
+#     last_response.body.should include("Sorry, something went wrong, please try again:")
+#   end    
+# 
+#   it "should return err message if URL isn't an URL" do
+#     post "/submit", params = {"url1"=>@bad_URL, "url2"=>@not_URL, "url_e"=>"", "freetext1"=>"", "freetext2"=>""}
+#     last_response.should be_ok
+#     last_response.body.should include("Sorry, something went wrong, please try again:")
+#   end    
+# end
